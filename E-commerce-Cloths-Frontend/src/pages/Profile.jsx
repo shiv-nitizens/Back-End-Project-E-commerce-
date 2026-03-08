5aa544c6ab6df9ea445099b4d2509e7ce5be75ed
Arunveer-RAAAAH <arunveer0862.becse24@chitkara.edu.in>
2026-03-08 19:55:06 +0530

    Did Profile Page UI

--- E-commerce-Cloths-Frontend/src/pages/Profile.jsx
+++ E-commerce-Cloths-Frontend/src/pages/Profile.jsx
@@ -2,25 +2,68 @@ import user from "../data/user.json";
 
 function Profile() {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-      
-      <div className="bg-white shadow-lg rounded-xl p-8 w-96">
-        
-        <h1 className="text-2xl font-bold mb-4">
-          {user.name}
-        </h1>
+    <div className="min-h-screen bg-gray-100 flex pt-24">
 
-        <p className="text-gray-600">
-          Email: {user.email}
-        </p>
+      {/* Sidebar */}
+      <div className="w-72 bg-white shadow-md p-8 rounded-lg">
 
-        <p className="text-gray-600">
-          Phone: {user.phone}
-        </p>
+  <h2 className="text-2xl font-bold mb-10">
+    My Account
+  </h2>
 
-        <p className="text-gray-600">
-          Address: {user.address}
-        </p>
+  <ul className="flex flex-col gap-8 text-lg font-medium">
+
+    <li className="cursor-pointer hover:text-black text-gray-600 transition">
+      Profile
+    </li>
+
+    <li className="cursor-pointer hover:text-black text-gray-600 transition">
+      Orders
+    </li>
+
+    <li className="cursor-pointer hover:text-black text-gray-600 transition">
+      Addresses
+    </li>
+
+    <li className="cursor-pointer hover:text-red-500 text-gray-600 transition">
+      Logout
+    </li>
+
+  </ul>
+
+</div>
+      {/* Profile Content */}
+      <div className="flex-1 p-10">
+
+        <div className="bg-white shadow-lg rounded-xl p-8 max-w-xl">
+
+          <h1 className="text-2xl font-bold mb-6">Profile Information</h1>
+
+          <div className="space-y-4">
+
+            <div>
+              <p className="text-gray-500 text-sm">Name</p>
+              <p className="text-lg">{user.name}</p>
+            </div>
+
+            <div>
+              <p className="text-gray-500 text-sm">Email</p>
+              <p className="text-lg">{user.email}</p>
+            </div>
+
+            <div>
+              <p className="text-gray-500 text-sm">Phone</p>
+              <p className="text-lg">{user.phone}</p>
+            </div>
+
+            <div>
+              <p className="text-gray-500 text-sm">Address</p>
+              <p className="text-lg">{user.address}</p>
+            </div>
+
+          </div>
+
+        </div>
 
       </div>
 
