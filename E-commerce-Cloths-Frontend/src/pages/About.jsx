0ec8b0f2c9bab7db0d58d951c35de6aeb4dfd641
Jayant <jayant0878.becse24@chitkara.edu.in>
2026-03-09 05:53:09 +0530

    Added a Socials Block and updated teamData with subsequent data

--- E-commerce-Cloths-Frontend/src/pages/About.jsx
+++ E-commerce-Cloths-Frontend/src/pages/About.jsx
@@ -23,25 +23,44 @@ const About = () => {
             {teamMembers.map((member) => (
             <div
                 key={member.id}
-                className="bg-white p-6 rounded-lg shadow hover:-translate-y-1 hover:shadow-xl transition"
+                className="relative group bg-white p-6 rounded-lg shadow text-center overflow-hidden"
             >
                 <h3 className="bold-18">{member.name}</h3>
                 <p className="bold-15">Contribution:</p>
                 <p className="text-gray-30">{member.Contribution}</p>
+
+                <div className="absolute inset-0 bg-gray-900 text-white flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition z-20">
+
+        <h4 className="font-semibold mb-3">Socials</h4>
+
+        <div className="flex gap-4 text-sm">
+          {member.github && (
+            <a
+              href={member.github}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-gray-30 transition"
+            >
+              GitHub
+            </a>
+          )}
+          </div>
+
+      </div>
             </div>
         ))}
 
         </div>
       </section>
 
-      <section className="flex justify-center gap-10 mb-16 text-center">
+      <section className="flex justify-center gap-10 text-center">
         <div>
             <h3 className="bold-24">4</h3>
             <p className="text-gray-30">Team Members</p>
         </div>
 
         <div>
-            <h3 className="bold-24">10+</h3>
+            <h3 className="bold-24">5+</h3>
             <p className="text-gray-30">Features Built</p>
         </div>
     </section>
